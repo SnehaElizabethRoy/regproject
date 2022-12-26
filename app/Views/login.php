58a7b83d3fcb24745420cb0ea8c896288f41dcd1
snehaelizabethroy <elizabethsneha@gmail.com>
2022-12-26 16:26:57 +0530

    reg_project

---
+++ app/Views/login.php
@@ -0,0 +1,29 @@
+<div class="container">
+<div class="row">
+    <div class="col-12 col-5m8 -offset-5m-2 col -md-6 offset -md-3 pb-3 bg-white from wrapper">    
+<div class="container"> 
+    <h3>Login</h3>
+    <hr>
+    <form class="" action= "/" method="post">
+    <div class="form-group"> 
+    <label for=" email">  Email Address </label>  
+    <input type="text" class="form-control" name="email" id="email" value=<?=set_value('email')?>> 
+</div>
+<div class="form-group"> 
+    <label for=" change"> Password</label>  
+    <input type="text" class="form-control" name="change" id="change" value="">  
+</div>   
+<div class="row">
+    <div class="col-12 col-8m-4">
+        <button type="submit" class="btn btn-primary" >Login</button>
+    </div>    
+    <div class= "col-12 col-6m-8 text-right"> 
+    <a href="/register">Don't have an account yet? </a>  
+</div> 
+</div>
+</form>
+</div>  
+</div>
+</div> 
+</div>
+
